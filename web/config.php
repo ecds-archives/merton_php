02ebb02957bf8ce52542138fd3b0e6310ceb0ca2
Rebecca Sutton Koeser <rebecca.s.koeser@emory.edu>
2005-08-03 20:12:53 +0000

    basic configuration for whole site (tamino database settings, etc.)

---
+++ web/config.php
@@ -0,0 +1,40 @@
+<?php
+
+/* Configuration settings for entire site */
+
+// set level of php error reporting -- turn off warnings when in production
+//error_reporting(E_ERROR | E_PARSE);
+
+
+// root directory and url where the website resides
+$server = "biliku.library.emory.edu";
+$base_path = "/rebecca/merton";
+$basedir = "/home/httpd/html$base_path";
+$base_url = "http://$server$base_path/";
+
+// add basedir to the php include path (for header/footer files and lib directory)
+set_include_path(get_include_path() . ":" . $basedir . ":" . "$basedir/lib");
+
+/* tamino settings  */
+$config{"server"} = "vip.library.emory.edu";
+$config{"db"} = "BECKCTR";
+$config{"coll"} = "merton";
+
+// base settings for all connections to exist
+$tamino_args = array('host'   => $config{"server"},
+		     'db'     => $config{"db"},
+		     'coll'   => $config{"coll"},
+		     'dbtype' => 'tamino');
+
+
+//Function that takes multiple terms separated by white spaces and puts them into an array
+function processterms ($str) {
+// clean up input so explode will work properly
+    $str = preg_replace("/\s+/", " ", $str);  // multiple white spaces become one space
+    $str = preg_replace("/\s$/", "", $str);	// ending white space is removed
+    $str = preg_replace("/^\s/", "", $str);  //beginning space is removed
+    $terms = explode(" ", $str);    // multiple search terms, divided by spaces
+    return $terms;
+}
+
+?>
