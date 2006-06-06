0378c2a9e5c6c941435acc582af956842cc7a5d2
Rebecca Sutton Koeser <rebecca.s.koeser@emory.edu>
2006-06-06 21:05:12 +0000

    minimal front page with intro paragraph

---
+++ web/index.php
@@ -0,0 +1,18 @@
+<?php
+print "<html>
+  <head> 
+    <link rel='stylesheet' type='text/css' href='merton.css'>
+    <title>Merton's Red Diary</title> 
+    <meta http-equiv='Content-Type' content='text/html; charset=iso-8859-1'> 
+  </head> 
+<body> 
+";
+
+include("nav.xml");
+include("header.xml");
+include("intro.xml");
+print "</div>
+    </body>
+    </html>";
+
+?>
