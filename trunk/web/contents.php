0f0f4abb7bd16acf895e0555820276b12a196541
rsutton <rsutton@7b602e88-806d-46b4-8461-ec180f495505>
2006-06-06 21:02:35 +0000

    added navigation / header to page

--- trunk/web/contents.php
+++ trunk/web/contents.php
@@ -3,6 +3,7 @@
 include("config.php");
 include_once("lib/xmlDbConnection.class.php");
 
+
 $myargs = $tamino_args;
 $myargs{"debug"} = false;
 $tamino = new xmlDbConnection($myargs);
@@ -22,5 +23,22 @@ $xsl = "contents.xsl";
 $tamino->xquery($xquery);
 $tamino->xslTransform($xsl);
 
-$tamino->printResult();
+print "<html>
+  <head> 
+    <link rel='stylesheet' type='text/css' href='merton.css'>
+    <title>Merton's Red Diary : Contents</title> 
+    <meta http-equiv='Content-Type' content='text/html; charset=iso-8859-1'> 
+    <base href='$base_url'> 
+  </head> 
+<body> 
+";
 
+include("nav.xml");
+include("header.xml");
+print "<div class='content'>";
+$tamino->printResult();
+print "</div>
+    </body>
+    </html>";
+
+?>
