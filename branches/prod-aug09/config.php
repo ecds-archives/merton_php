483ff153b83726fb9ebbd9ce67c0fe314793f732
ahickco <ahickco@7b602e88-806d-46b4-8461-ec180f495505>
2009-08-21 17:26:44 +0000

    temp change to config for testing

--- branches/prod-aug09/config.php
+++ branches/prod-aug09/config.php
@@ -27,7 +27,7 @@ $base_url = "http://$server$base_path/";
 */
 
 //production
-$base_path = "/merton";
+$base_path = "/merton-new";
 $basedir = "home/httpd/html/beck/$base_path";
 $base_url = "http://$server$basepath";
 
