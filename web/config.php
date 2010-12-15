f9a7f2aa21ae11388ca28ae362311671101a02e6
cdunne <cdunne@7b602e88-806d-46b4-8461-ec180f495505>
2010-12-15 19:05:57 +0000

    ahicko revisions to allow cdunne to view changes on local host

--- web/config.php
+++ web/config.php
@@ -1,10 +1,10 @@
 <?php
 
 /* Configuration settings for entire site */
-$in_production = true;
+$in_production = false;
 
 // set level of php error reporting -- turn off warnings when in production
-if($in_production) {
+if($in_production=="true") {
   error_reporting(E_ERROR);
  } else {
 error_reporting(E_ERROR | E_PARSE);
@@ -16,16 +16,16 @@ $base_path = "/rebecca/merton";
 $basedir = "/home/httpd/html$base_path";
 $base_url = "http://$server$base_path/"; */
 
-if ($in_production) {
+if ($in_production=="true") {
   $server = "beck.library.emory.edu";           //production
   $base_path = "/merton";
   $basedir = "/home/httpd/html/beck$base_path";
   $base_url = "http://beck.library.emory.edu$base_path/";
 
 } else {
-  $server = "wilson.library.emory.edu";         // test
-  $base_path = "/~ahickco/merton";
-  $basedir = "/home/ahickco/public_html/merton";
+  $server = "kamina.library.emory.edu";         // test
+  $base_path = "/~cdunne/merton";
+  $basedir = "/Users/cdunne/Sites/merton";
   $base_url = "http://$server$base_path/";
  }
 
@@ -46,7 +46,7 @@ set_include_path(get_include_path() . ":" . $basedir . ":" . "$basedir/lib");
 		     'dbtype' => 'tamino');*/
 
 /* eXist settings */
-if($in_production) {
+if($in_production=="true") {
   $port = "7080";
  } else {
 $port = "8080";
